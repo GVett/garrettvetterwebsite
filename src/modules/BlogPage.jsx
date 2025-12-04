4d5cb411223b2bf18372264fb851d51fe7daed38
GVett <garrettvetter@icloud.com>
2025-12-04 00:37:54 -0800

    Favicons rounded

---
+++ src/modules/BlogPage.jsx
@@ -0,0 +1,17 @@
+function HomePage(){
+    return (
+        <>
+            <h2>Life of Gurt</h2>
+            <article>
+                <h3>BLog</h3>
+                <p>
+                    
+                </p>
+                <p>
+                    Yurr
+                </p>
+            </article>
+        </>
+    );
+}
+export default HomePage;
